e71d6b7aefe439115de338fff0547d5618134ac1
erylieclark <erylieclark@gmail.com>
2021-07-21 18:46:19 -0700

    42%

--- biogimmickry/biogimmickry.py
+++ biogimmickry/biogimmickry.py
@@ -138,8 +138,10 @@ def create_parent(max_len: int, loop: int) -> str:
     program: str = ""
     all_chars: List[str] = []
     all_weights: List[int] = []
-    char_choice: List[str] = []
-    weight_choice: List[int] = []
+    loop_ended: int = False
+    i: int = 0
+    #char_choice: List[str] = []
+    #weight_choice: List[int] = []
 
     # Make the selection of characters to choose from
     if loop:        # A Loop should be involved
@@ -149,12 +151,13 @@ def create_parent(max_len: int, loop: int) -> str:
         weight_choice = all_weights[0:3]
     else:           # No loop involved
         all_chars = ["+", "-", ">"]
-        all_weights = [8, 8, 1]
+        all_weights = [3, 3, 1]
         char_choice = all_chars
         weight_choice = all_weights
+        # loop_ended = True
     # Starting with a "<" or "[" is useless and cannot start with "]"
 
-    for _ in range(1, length):
+    while i < length or loop_ended:
         # Fill in the next program character
         program = program + random.choices(char_choice, \
             weights=weight_choice)[0]
@@ -163,6 +166,9 @@ def create_parent(max_len: int, loop: int) -> str:
         weight_choice = all_weights.copy()
         char_choice, weight_choice = check_last_move(char_choice, \
             weight_choice, program[-1], loop)
+        if program[-1] == "]":
+            loop_ended = True
+        i += 1
 
     return program
 
@@ -194,42 +200,57 @@ def sort_and_slice(parents: List[str], scores: List[float], pcnt_keep: float) \
     the lowest probabilities that the program will be used for creating the
     next generation.
     """
-    #print("Num parents started with: ", len(scores))
-    #for i in range(len(scores)):
-    #    print("Parent: ", parents[i])
-    #    print("    Score: {:0.2f}".format(scores[i]))
-    #print("************************* Sorting **********************")
     # Sort the parents based on the scores
     parents = [x for _, x in sorted(zip(scores, parents), reverse=True)]
     # Sort the scores
     scores.sort(reverse=True)
-    #for i in range(len(scores)):
-        #print("Parent: ", parents[i])
-        #print("    Score: {:0.3f}".format(scores[i]))
-
-    #split_idx = list(filter(lambda i: i < th, scores))[0]
-    #split_idx = next(x for x, val in enumerate(scores) if val < th)
     split_idx = round(len(scores)*pcnt_keep)
-    #print("Split_idx: ", split_idx)
-    #print("************************ Slicing ***********************")
     parents = parents[:split_idx]
     scores = scores[:split_idx]
-    #for i in range(len(scores)):
-    #    print("Parent: ", parents[i])
-    #    print("    Score: {:0.2f}".format(scores[i]))
-    #print(" Num parents ending with: ", len(scores))
     
     return parents, scores
 
 
-def crossover(fe: FitnessEvaluator, parents: List[str], scores: List[float], \
-elite_pcnt: float) -> List[str]:
+def cross_no_loop(p1: str, p2: str) -> Tuple[str, str]:
     """
     Keep the top "elite" percent and transfer them directly to the next
     generation, then take the remainder and use them for crossover.
     """
-    children: List[str] = [""]*len(parents)
-    #child_scores: List[int] = [0]*len(parents)
+    #i: int = 0 
+    c1: str = ""
+    c2: str = ""
+    idx_list: List[int] = []
+    #print("Parent 1: ", p1)
+    #print("Parent 2: ", p2)
+
+    #idx_list = [i for i, letter in enumerate(p1) if letter == ">"]
+    if len(idx_list) < 1:
+        idx = random.randint(0, len(p1))
+    else:
+        idx = random.choice(idx_list)
+    #print("P1 split idx: ", idx)
+    c1 = p1[:idx] 
+    c2 = p1[idx:]
+    #idx_list = [i for i, letter in enumerate(p2) if letter == ">"]
+    if len(idx_list) < 1:
+        idx = random.randint(0, len(p2))
+    else:
+        idx = random.choice(idx_list)
+    #print("P2 split idx: ", idx)
+    c1 = c1 + p2[idx:] 
+    c2 = p2[:idx] + c2
+    #print("C1 after P2: ", c1)
+    #print("C2 after P2: ", c2)
+    return c1, c2
+
+
+def crossover(fe: FitnessEvaluator, parents: List[str], scores: List[float], \
+elite_pcnt: float, loop: int, max_len: int) -> List[str]:
+    """
+    Keep the top "elite" percent and transfer them directly to the next
+    generation, then take the remainder and use them for crossover.
+    """
+    children = parents
     p1: str = ""
     p2: str = ""
     c1: str = ""
@@ -239,18 +260,19 @@ elite_pcnt: float) -> List[str]:
     # If remainder is odd, add one to elite so that crossover has even
     if (len(parents) - num_elite) % 2:
         num_elite = num_elite + 1
-    # Copy the elites to the children
-    children[:num_elite] = parents[:num_elite]
-    #child_scores[:num_elite] = scores[:num_elite]
-    #print("Children: ", children)
     # Use the parents to create new children using crossover
     for i in range(int((len(parents)-num_elite)/2)):
         p1, p2 = random.choices(parents, k=2, weights=scores)
         # Cross the pairs
-        split_idx = random.randint(0, min(len(p1), len(p2)))
-        #print("Split idx: ", split_idx)
-        c1 = p1[:split_idx] + p2[split_idx:]
-        c2 = p2[:split_idx] + p1[split_idx:]
+        if loop:
+            print("Contains a loop.")
+            #c1, c2 = cross_loop(p1, p2)
+        else:
+            c1, c2 = cross_no_loop(p1, p2)
+            if len(c1) > max_len:
+                c1 = c1[:max_len]
+            if len(c2) > max_len:
+                c2 = c2[:max_len]
         children[i*2+num_elite] = c1
         children[i*2+1+num_elite] = c2
         
@@ -277,6 +299,12 @@ def mutate_weird_sequence(program: str, loop: int) -> str:
     if idx != -1:
         program = program[:idx+1] + program[idx] + \
             program[idx+2:]
+    idx = program.find("+-")
+    if idx != -1:
+        program = program[:idx]
+    idx = program.find("-+")
+    if idx != -1:
+        program = program[:idx]
     #if (idx := program.find("<<")) != -1:
     #    program = program[:idx] + program[idx-1] + \
     #        program[idx+1:]
@@ -290,16 +318,92 @@ def mutate_weird_sequence(program: str, loop: int) -> str:
                 program[idx+1:]
         
     return program
+
         
+def mutate_add(program: str, loop: int) -> str:
+    """
+    Mutate
+    """
+    all_chars: List[str] = [""]
+    all_weights: List[int] = [0]
+    i: int = 0
+    idx: int = 0
+    num_muts: int = round(len(program)*0.1)
+    
+    if loop:
+        all_chars = ["+", "-", ">", "<", "[", "]"]
+        all_weights = [8, 8, 1, 1, 4, 4]
+    else:           # No loop involved
+        all_chars = ["+", "-", ">"]
+        all_weights = [1, 1, 1]
+    
+    while i < num_muts:
+        idx = random.randint(0, len(program))
+        add_char = random.choices(all_chars, weights=all_weights)[0]
+        program = program[:idx] + add_char + program[idx:]
+        i += 1
+    
+    return program
+
+
+
+def mutate_remove(program: str, loop: int) -> str:
+    """
+    Mutate 
+    """
+    num_muts: int = round(len(program)*0.1)
+    i: int = 0
+    idx: int = 0
+    while i < num_muts:
+        idx = random.randint(0, len(program))
+        program = program[:idx] + program[idx+1:]
+        i += 1
+    return program
+
+
+def mutate_replace(program: str, loop: int) -> str:
+    """
+    Mutate 
+    """
+    all_chars: List[str] = [""]
+    all_weights: List[int] = [0]
+    i: int = 0
+    idx: int = 0
+    num_muts: int = round(len(program)*0.1)
+    
+    if loop:
+        all_chars = ["+", "-", ">", "<", "[", "]"]
+        all_weights = [8, 8, 1, 1, 4, 4]
+    else:           # No loop involved
+        all_chars = ["+", "-", ">"]
+        all_weights = [3, 3, 1]
+    
+    while i < num_muts:
+        idx = random.randint(0, len(program))
+        add_char = random.choices(all_chars, weights=all_weights)[0]
+        program = program[:idx] + add_char + program[idx+1:]
+        i += 1
+
+    return program
+
+
+
 def mutate(children: List[str], init_pop: int, cur_pop: int, loop: int) \
 -> List[str]:
     """
     Mutate -> Increase the mutation rate as the population gets smaller
     """
-    sus_seqs: List[str] = ["<>", "><", "+-+", "-+-", "[]"]
+    sus_seqs: List[str] = ["<>", "><", "+-+", "-+-", "+-", "-+", "[]"]
+    muts: List[str] = ["+", "-", "r"]
+    weights: List[int] = []
     mutate: str = ""
     idx: int = 0
     num_mutate = round((init_pop - cur_pop)/init_pop*cur_pop)
+
+    if loop:
+        weights = [3, 6, 2]
+    else: 
+        weights = [6, 1, 4]
     #print("Current Pop: ", cur_pop)
     #print("Num Mutations: ", num_mutate)
     for _ in range(num_mutate):
@@ -310,11 +414,63 @@ def mutate(children: List[str], init_pop: int, cur_pop: int, loop: int) \
             #print("Found Sus Sequence.")
             mutate = mutate_weird_sequence(mutate, loop)
             #print("Mutated Sus Sequence: ", mutate)
+        next_mut = random.choices(muts, weights=weights)[0]
+        #print("Next mutation: ", next_mut)
+        if next_mut == "-":
+            mutate = mutate_remove(mutate, loop)
+        elif next_mut == "+":
+            mutate = mutate_add(mutate, loop)
+        elif next_mut == "r":
+            mutate = mutate_replace(mutate, loop)
         children[idx] = mutate
 
-
-
     return children
+
+
+def eval_new_parents(fe: FitnessEvaluator, parents: List[str], \
+scores: List[int]) -> Tuple[List[str], List[int], int]:
+    """
+    evaluate the new parents created
+    """
+    j: int = 0
+
+    while j < len(parents):
+        try:
+            scores[j] = fe.evaluate(parents[j])     # Evaluate it
+        except RuntimeError:
+            del parents[j]
+            scores.pop()
+            continue
+        if not scores[j]:   # If we get lucky with one that succeeds
+            return parents, scores, j
+        j += 1
+    return parents, scores, -1
+
+
+def check_startover(parents: List[str], scores: List[int], count: int, \
+init_pop: int, max_len: int, loop: int) -> Tuple[List[str], List[int], int]:
+    """
+    check_startover - checks for convergence of the scores to be within too
+    small of a range, or if the population size has gotten too small. Will
+    start over if either of these occur
+    """
+    init_parents: List[str] = [""]*init_pop
+    init_scores: List[int] = [0]*init_pop
+
+    # Check for convergence by seeing if the range of scores is small
+    if max(scores)-min(scores) < 5:
+        count += 1      # Allow it to continue a few more times
+    else:
+        count = 0
+    if len(parents) < round(init_pop*0.01) or count > 10: 
+        parents = init_parents.copy()   # Reset the parents to init pop size
+        scores = init_scores.copy()     # Reset scores to init pop size
+        count = 0                       # Reset means not converging anymore
+        for i in range(init_pop):       # Create the new parents
+            parents[i] = create_parent(max_len, loop)  
+            
+    #print("Initial Population: ", parents)
+    return parents, scores, count
 
 
 def create_program(fe: FitnessEvaluator, max_len: int) -> str:
@@ -324,77 +480,59 @@ def create_program(fe: FitnessEvaluator, max_len: int) -> str:
 
     Use fe.evaluate(program) to get a program's fitness score (zero is best).
     """
-    init_pop: int = 500
-    init_parents: List[str] = [""]*init_pop
+    init_pop: int = 1500
     parents: List[str] = [""]
     scores: List[int] = [0]
-    init_scores: List[int] = [0]*init_pop
-    #norm_scores: List[float] = []
-    #total: int = 0
+    #avg_score: float = 100
+    count: int = 0
     #pcnt_keep: float = 0.98
     #elitism: float = 0.02
-    j: int = 0
+    idx: int = 0
     loop: int = False
-    #count: int = 0
 
     if max_len:
         loop = True
         return "+"
     else:
-        max_len = 30
+        max_len = 40
         
     # Get the parents for the starting population    
     
     while True:
-        #print("-------------------------- New Loop ---------------------")
-        #print("Length of population: ", len(parents))
-        if len(parents) < 200:
-            parents = init_parents.copy()
-            scores = init_scores.copy()
-            for i in range(init_pop):
-                parents[i] = create_parent(max_len, loop)   # Create the parent
-            #print("New Len of Parents: ", len(parents))
-        j = 0
-        #count = 0
-        while j < len(parents):
-            try:
-                scores[j] = fe.evaluate(parents[j])     # Evaluate it
-                #print("j = ", j)
-            except RuntimeError:
-                #print("Runtime Error")
-                del parents[j]
-                scores.pop()
-                #count += 1
-                continue
-            if not scores[j]:   # If we get lucky with one that succeeds
-                return parents[j]
-            j += 1
+        if len(parents) < round(init_pop*0.01) or (max(scores)-min(scores)) < 5:
+            parents, scores, count = check_startover(parents, scores, count,\
+                init_pop, max_len, loop)
+        
+        # Evaluate the newly generated parents and return the scores
+        parents, scores, idx = eval_new_parents(fe, parents, scores)
+        if idx != -1:
+            return parents[idx]
         #print(scores)
-        #print("Count: ", count)
-        #print("Parent Length: ", len(parents))
-        #print("Scores Length: ", len(scores))
+
         # Add the scores up and normalize them
         norm_scores = normalize_scores(scores)
 
         # Sort the parents based on their scores
         # Use a threshold to weed some out
+        #print("Parent Length before slice: ", len(parents))
         parents, norm_scores = sort_and_slice(parents, norm_scores, 0.98)
+        #print("Parent Length after slice: ", len(parents))
 
         # Pick pairs out of the rest to use for crossover
-        children = crossover(fe, parents, norm_scores, 0.02)
+        children = crossover(fe, parents, norm_scores, 0.05, loop, max_len)
+        
+        # Mutate some of the crossed pairs
         parents = mutate(children, init_pop, len(children), loop)
-        #parents = children
+        
         # Shorten the scores to the length of the children
         scores = scores[:len(children)]
         #print("Children: ", children)
-        # Mutate some of the crossed pairs
-        # Evaluate new set
     
     
     return parents[0]
 
 def main() -> None:  # optional driver
-    array = (-1, 2, -3, 4)
+    array = (-1, 2, -3, 4, -4, 3, 2) #, -4, 3, 2, -1)
     max_len = 0  # no BF loop required
 
     # only attempt when non-loop programs work
